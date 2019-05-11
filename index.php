fcf14e6d61b1a100d03a6b91c39b80b7a5b68ece
Varun <varunsridharan23@gmail.com>
2019-05-11 06:37:14 +0530

    Created Base For the addon.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WPOnion Conditional Logic Builder
+ * Plugin URI: https://github.com/wponion/conditional-logic-builder-addon
+ * Description: Simple Addon Which Providers An Interface for End Users To Build Custom Logis Based On Their Needs.
+ * Version: 0.1
+ * Author: varunsridharan
+ * Author URI: https://varunsridharan.in
+ * Text Domain: wponion-conditional-logic-builder-addon
+ * Domain Path: /i18n/
+ */
